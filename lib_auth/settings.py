2dbffb611b7c5d62d2fe5dae812af18106bd607f
froggy <thefrogwhoseesall@gmail.com>
2021-12-24 21:46:24 +0530

    requirments

--- lib_auth/settings.py
+++ lib_auth/settings.py
@@ -143,7 +143,7 @@ SOCIALACCOUNT_EMAIL_REQUIRED = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
