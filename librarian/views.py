a83aa789e86e84d21b9b96f2030ff4ca6a0f4e5f
froggy <thefrogwhoseesall@gmail.com>
2021-12-25 15:42:01 +0530

    extension

--- librarian/views.py
+++ librarian/views.py
@@ -3,7 +3,6 @@ from lib_app.models import Issue, Book, Renew
 from lib_app.forms import UploadFileForm
 from django.contrib.auth import authenticate, login, logout
 from datetime import datetime, timedelta
-from django.core.mail import send_mail
 from lib_auth.settings import EMAIL_HOST_PASSWORD, EMAIL_HOST_USER
 from librarian.forms import BookForm
 import smtplib
@@ -44,8 +43,12 @@ def logging(request):
         return render(request, "error.html")
     elif(request.FILES):
         form = UploadFileForm(request.POST, request.FILES)
+        print('-1')
         if form.is_valid():
+            print('-2')
             handle_uploaded_file(request.user,request.FILES['file'])
+        else:
+            print(form._errors)
     elif(request.POST.get('time')):
         time = request.POST.get('time')
         issue_id = request.POST.get('issue_id')
@@ -117,29 +120,11 @@ def logging(request):
             issued.due_date = issued.due_date + td
             issued.save()
             renew.delete()
-            msg = "Subject: Renew request aceepted\n\n Due date has been furthered by "+td+" day/s"
-            email = SocialAccount.objects.filter(
-                user=issued.user).first().extra_data['email']
-            with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-                smtp.ehlo()
-                smtp.starttls()
-                smtp.ehlo()
-                smtp.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
-                smtp.sendmail(EMAIL_HOST_USER, email, msg)
         except:
             pass    
     elif(request.POST.get('decline')):
         try:    
             renew = Renew.objects.get(id=request.POST.get('timed'))
-            msg = "Subject: Renew request Declined\n\n Your renew request has been declined "
-            email = SocialAccount.objects.filter(
-                user=renew.issue.user).first().extra_data['email']
-            with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-                smtp.ehlo()
-                smtp.starttls()
-                smtp.ehlo()
-                smtp.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
-                smtp.sendmail(EMAIL_HOST_USER, email, msg)
             renew.delete()
         except:
             pass    
@@ -161,15 +146,18 @@ def libInterface(request):
 
 def handle_uploaded_file(user,spreadsheet):
     fs = FileSystemStorage()
+    print('0')
     fs.save(r'spreadsheet\bookdata.xlsx', spreadsheet)
     workbook = load_workbook(filename=r"media\spreadsheet\bookdata.xlsx")
     sheet = workbook.active
     # 0name-1image_link-2summary-3author-4genre-5isbn-6location
     for value in sheet.iter_rows(min_row=1,min_col=1,max_col=7,values_only=True):
+        print('1')
         if not(value[5]):
             break
         elif(Book.objects.filter(isbn=value[5]).exists()):
             book = Book.objects.filter(isbn=value[5]).first()
+            print('2')
             if(value[0]):
                 book.name = value[0]
             if(value[1]):
@@ -184,12 +172,13 @@ def handle_uploaded_file(user,spreadsheet):
                 book.location = value[6]
             book.save()
         else:
+            print('3')
             book = Book.objects.create(name=value[0], image_link = value[1], summary=value[2], author=value[3], genre=value[4], isbn=value[5], location=value[6])
             book.save()
             user.profile.lib_data.books_added +=1
             user.profile.lib_data.save()
-        import shutil
-        shutil.rmtree(r"media\spreadsheet")
+            import shutil
+            shutil.rmtree(r"media\spreadsheet")
 
 def profile(request):
     current_user = request.user
