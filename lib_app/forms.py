5aba86b39e5f54d7468223f15a5126f000f879e1
froggy <thefrogwhoseesall@gmail.com>
2022-01-07 23:44:25 +0530

    l

--- lib_app/forms.py
+++ lib_app/forms.py
@@ -15,7 +15,7 @@ class RatingForm(forms.ModelForm):
 
 class ProfileForm(forms.Form):
     bits_id = forms.CharField(max_length=13, min_length=13, validators=[RegexValidator(regex=r"\d\d\d\d\D\d\D\D\d\d\d\d\D", message="Invalid BITS-ID")])
-    hostel = forms.CharField(max_length=50, validators=[RegexValidator(regex=r'[a-zA-Z\s]')])
+    hostel = forms.CharField(max_length=50, validators=[RegexValidator(regex=r'^[a-zA-Z\s]+$')])
     room = forms.IntegerField(max_value=999, min_value=1)
     phone_number = forms.IntegerField(max_value=9999999999, min_value=1000000000)
     
